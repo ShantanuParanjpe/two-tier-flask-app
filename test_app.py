40861c5bbcb237eecd5bcd38d72fb83e94c76704
Your Name <you@example.com>
2025-09-06 21:42:01 +0100

    Added changes in test_app.py

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import pytest
 from unittest.mock import MagicMock, patch
-from app import app, init_db
+from app import app, init_db, mysql
 
 @pytest.fixture
 def client():
